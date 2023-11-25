56e49ae0050d13983cefaec53d0fe7da98fb8efd
Rich Pecson <richard_pecson@dlsu.edu.ph>
2023-11-25 21:29:53 +0800

    modified functions

--- MP2/main.c
+++ MP2/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void imgAvgFilter(int* input_image, int* filtered_image, int image_size_x, int image_size_y, int sampling_window_size) {
-    for (int i = 0; i < image_size_x; i++) {
-        for (int j = 0; j < image_size_y; j++) {
+void imgAvgFilter(int* input_image, int* filtered_image, int image_size_x, int image_size_y, int sampling_window_size) {
+  int i, j, k, l;
+    for (i = 0; i < image_size_x; i++) {
+        for (j = 0; j < image_size_y; j++) {
             int sum = 0;
             int count = 0;
-            for (int k = -sampling_window_size; k <= sampling_window_size; k++) {
-                for (int l = -sampling_window_size; l <= sampling_window_size; l++) {
+            for (k = -sampling_window_size; k <= sampling_window_size; k++) {
+                for (l = -sampling_window_size; l <= sampling_window_size; l++) {
                     int x = i + k;
                     int y = j + l;
                     if (x >= 0 && x < image_size_x && y >= 0 && y < image_size_y) {
@@ -22,10 +23,32 @@ extern void imgAvgFilter(int* input_image, int* filtered_image, int image_size_x
 }
 
 void printImage(int* image, int image_size_x, int image_size_y) {
-    for (int i = 0; i < image_size_x; i++) {
-        for (int j = 0; j < image_size_y; j++) {
+    int i, j;
+    for (i = 0; i < image_size_x; i++) {
+        for (j = 0; j < image_size_y; j++) {
             printf("%d ", image[i * image_size_y + j]);
         }
         printf("\n");
     }
+}
+
+int main(int argc, char** argv) {
+    int image_size_x = 5;
+    int image_size_y = 5;
+    int sampling_window_size = 1;
+    int* input_image = (int*)malloc(image_size_x * image_size_y * sizeof(int));
+    int* filtered_image = (int*)malloc(image_size_x * image_size_y * sizeof(int));
+    int i, j;
+    for (i = 0; i < image_size_x; i++) {
+        for (j = 0; j < image_size_y; j++) {
+            input_image[i * image_size_y + j] = i * image_size_y + j;
+        }
+    }
+    imgAvgFilter(input_image, filtered_image, image_size_x, image_size_y, sampling_window_size);
+    printImage(input_image, image_size_x, image_size_y);
+    printf("\n");
+    printImage(filtered_image, image_size_x, image_size_y);
+    free(input_image);
+    free(filtered_image);
+    return 0;
 }
