b3443356d5101154ab339e0cece5d9d35e2126cc
richjpex <richard_pecson@dlsu.edu.ph>
2023-11-28 20:02:03 +0800

    capitalized

--- MP2/imgAvgFilter.asm
+++ MP2/imgAvgFilter.asm
@@ -18,163 +18,163 @@ global _imgAvgFilter
 
 _imgAvgFilter:
     push ebp
-    mov ebp, esp
+    MOV ebp, esp
 
     ; Initializing variables
-    mov esi, [ebp+8]   ; input_image
-    mov dword [input_image_pointer], esi
+    MOV esi, [ebp+8]   ; input_image
+    MOV dword [input_image_pointer], esi
 
-    mov esi, [ebp+12]  ; filtered_image
-    mov dword [filtered_image_pointer], esi
+    MOV esi, [ebp+12]  ; filtered_image
+    MOV dword [filtered_image_pointer], esi
 
-    mov esi, [ebp+16]  ; image_size_x
-    mov dword [image_size_x], esi
+    MOV esi, [ebp+16]  ; image_size_x
+    MOV dword [image_size_x], esi
 
-    mov esi, [ebp+20]  ; image_size_y
-    mov dword [image_size_y], esi
+    MOV esi, [ebp+20]  ; image_size_y
+    MOV dword [image_size_y], esi
 
-    mov esi, [ebp+24]  ; sampling_window_size
-    mov dword [sampling_window_size], esi
+    MOV esi, [ebp+24]  ; sampling_window_size
+    MOV dword [sampling_window_size], esi
 
-    mov dword [i], 0
-    mov dword [j], 0
-    mov dword [border], 0
+    MOV dword [i], 0
+    MOV dword [j], 0
+    MOV dword [border], 0
 
-    mov eax, [sampling_window_size]
-    mov ebx, 2
-    div ebx
-    dec eax
-    mov [border], eax
+    MOV eax, [sampling_window_size]
+    MOV ebx, 2
+    DIV ebx
+    DEC eax
+    MOV [border], eax
 
 for_row:
-    mov eax, [i]
-    cmp eax, [image_size_x]
-    jge for_row_end
+    MOV eax, [i]
+    CMP eax, [image_size_x]
+    JGE for_row_end
 
-    mov dword [j], 0
+    MOV dword [j], 0
 
 for_col:
-    mov eax, [j]
-    cmp eax, [image_size_y]
-    jge for_col_end
+    MOV eax, [j]
+    CMP eax, [image_size_y]
+    JGE for_col_end
 
-    mov eax, [i]
-    cmp eax, [border]
-    jle true_condition
+    MOV eax, [i]
+    CMP eax, [border]
+    JLE true_condition
 
-    mov ebx, [image_size_x]
-    dec ebx
-    sub ebx, [border]
-    cmp eax, ebx
-    jge true_condition
+    MOV ebx, [image_size_x]
+    DEC ebx
+    SUB ebx, [border]
+    CMP eax, ebx
+    JGE true_condition
 
-    mov eax, [j]
-    cmp eax, [border]
-    jle true_condition
+    MOV eax, [j]
+    CMP eax, [border]
+    JLE true_condition
 
-    mov ebx, [image_size_y]
-    dec ebx
-    sub ebx, [border]
-    cmp eax, ebx
-    jge true_condition
+    MOV ebx, [image_size_y]
+    DEC ebx
+    SUB ebx, [border]
+    CMP eax, ebx
+    JGE true_condition
 
-    jmp false_condition
+    JMP false_condition
 
     true_condition:
-        mov eax, [i]
-        mov ebx, [image_size_x]
-        mul ebx
-        add eax, [j]
-        shl eax, 2
-        mov esi, [input_image_pointer]
-        add esi, eax
+        MOV eax, [i]
+        MOV ebx, [image_size_x]
+        MUL ebx
+        ADD eax, [j]
+        SHL eax, 2
+        MOV esi, [input_image_pointer]
+        ADD esi, eax
 
-        mov edi, [filtered_image_pointer]
-        add edi, eax
+        MOV edi, [filtered_image_pointer]
+        ADD edi, eax
 
-        mov eax, [esi]
-        mov [edi], eax
-        jmp end_if_condition
+        MOV eax, [esi]
+        MOV [edi], eax
+        JMP end_if_condition
 
     false_condition:
-        mov dword [sum], 0
-        mov dword [k], 0 
+        MOV dword [sum], 0
+        MOV dword [k], 0 
 
 for_row_sampling:
-        mov eax, [k]
-        cmp eax, [sampling_window_size]
-        jge for_row_sampling_end
+        MOV eax, [k]
+        CMP eax, [sampling_window_size]
+        JGE for_row_sampling_end
 
-        mov dword [l], 0
+        MOV dword [l], 0
 
 for_col_sampling:
-            mov eax, [l]
-            cmp eax, [sampling_window_size]
-            jge for_col_sampling_end
+            MOV eax, [l]
+            CMP eax, [sampling_window_size]
+            JGE for_col_sampling_end
 
-            mov eax, [i]
-            mov ecx, [border]
-            inc ecx
-            sub eax, ecx
-            add eax, [k]
-            mov ebx, [image_size_x]
-            mul ebx
-            mov ebx, [j]
-            sub ebx, ecx
-            add ebx, [l]
-            add eax, ebx
-            shl eax, 2
-            mov esi, [input_image_pointer]
-            add esi, eax
+            MOV eax, [i]
+            MOV ecx, [border]
+            INC ecx
+            SUB eax, ecx
+            ADD eax, [k]
+            MOV ebx, [image_size_x]
+            MUL ebx
+            MOV ebx, [j]
+            SUB ebx, ecx
+            ADD ebx, [l]
+            ADD eax, ebx
+            SHL eax, 2
+            MOV esi, [input_image_pointer]
+            ADD esi, eax
 
-            mov eax, [sum]
-            add eax, [esi]
-            mov [sum], eax
+            MOV eax, [sum]
+            ADD eax, [esi]
+            MOV [sum], eax
 
-            inc dword [l]
-            jmp for_col_sampling
+            INC dword [l]
+            JMP for_col_sampling
 
 for_col_sampling_end:
-        inc dword [k]
-        jmp for_row_sampling
+        INC dword [k]
+        JMP for_row_sampling
 
 for_row_sampling_end:
-        mov eax, [sampling_window_size]
-        mov ebx, [sampling_window_size]
-        mul ebx
-        mov ebx, eax
-        mov eax, [sum]
-        mov ecx, ebx
+        MOV eax, [sampling_window_size]
+        MOV ebx, [sampling_window_size]
+        MUL ebx
+        MOV ebx, eax
+        MOV eax, [sum]
+        MOV ecx, ebx
         shr ebx, 1
-        add eax, ebx
-        mov ebx, ecx
-        div ebx
+        ADD eax, ebx
+        MOV ebx, ecx
+        DIV ebx
 
-        mov ecx, eax
-        mov eax, [i]
-        mov ebx, [image_size_x]
-        mul ebx
-        add eax, [j]
-        shl eax, 2
-        mov edi, [filtered_image_pointer]
-        add edi, eax
+        MOV ecx, eax
+        MOV eax, [i]
+        MOV ebx, [image_size_x]
+        MUL ebx
+        ADD eax, [j]
+        SHL eax, 2
+        MOV edi, [filtered_image_pointer]
+        ADD edi, eax
 
-        mov eax, ecx
-        mov [edi], eax
+        MOV eax, ecx
+        MOV [edi], eax
 
 end_if_condition:
-        mov eax, [j]
-        inc eax
-        mov [j], eax
-        jmp for_col
+        MOV eax, [j]
+        INC eax
+        MOV [j], eax
+        JMP for_col
 
 for_col_end:
-    mov eax, [i]
-    inc eax
-    mov [i], eax
-    jmp for_row
+    MOV eax, [i]
+    INC eax
+    MOV [i], eax
+    JMP for_row
 
 for_row_end:
-    mov esp, ebp
+    MOV esp, ebp
     pop ebp
     ret
