a09f3338795fbe88b7770531588196294213bd3b
richjpex <richard_pecson@dlsu.edu.ph>
2023-11-28 19:26:35 +0800

    modified assembly code v1

--- MP2/README.md
+++ MP2/README.md
@@ -7,3 +7,23 @@ gcc -c main.c -o main.obj -m32
 gcc main.obj imgAvgFilter.obj -o main.exe -m32
 
 main.exe```
+
+## Pseudocode
+```
+function imgAvgFilter(input_image, filtered_image, image_size_x, image_size_y, sampling_window_size):
+    border = (sampling_window_size - 1) / 2
+    
+    for i in range(image_size_x):
+        for j in range(image_size_y):
+            if border < i < (image_size_x - border - 1) and border < j < (image_size_y - border - 1):
+                filtered_image[i, j] = input_image[i, j]
+            else:
+                sum = 0
+                for k in range(sampling_window_size):
+                    for l in range(sampling_window_size):
+                        sum += input_image[i - (border + 1) + k, j - (border + 1) + l]
+                
+                average = round(sum / (sampling_window_size * sampling_window_size))
+                filtered_image[i, j] = average
+
+    return filtered_image```
--- MP2/imgAvgFilter.asm
+++ MP2/imgAvgFilter.asm
@@ -1,200 +1,175 @@
-global _imgAvgFilter
-
-segment .data
+section .data
     sum dd 0
 
-segment .bss
-    ; parameter variables
+section .bss
     input_image_pointer resd 1
     filtered_image_pointer resd 1
     image_size_x resd 1
     image_size_y resd 1
     sampling_window_size resd 1
-
-    ; local variables
     border resd 1
-
-    ; loop variables
     i resd 1
     j resd 1
-
     k resd 1
     l resd 1
 
-segment .text
+section .text
+global _imgAvgFilter
+
 _imgAvgFilter:
     push ebp
     mov ebp, esp
-    
-    mov esi, [ebp+8] ; input_image
+
+    mov esi, [ebp+8]   ; input_image
     mov dword [input_image_pointer], esi
-    mov esi, [ebp+12] ; filtered_image
+    mov esi, [ebp+12]  ; filtered_image
     mov dword [filtered_image_pointer], esi
-    mov esi, [ebp+16] ; image_size_x
+    mov esi, [ebp+16]  ; image_size_x
     mov dword [image_size_x], esi
-    mov esi, [ebp+20] ; image_size_y
+    mov esi, [ebp+20]  ; image_size_y
     mov dword [image_size_y], esi
-    mov esi, [ebp+24] ; sampling_window_size
+    mov esi, [ebp+24]  ; sampling_window_size
     mov dword [sampling_window_size], esi
 
-    mov dword [i], 0 ; initialize i to 0
-    mov dword [j], 0 ; initialize j to 0
+    mov dword [i], 0
+    mov dword [j], 0
+    mov dword [border], 0
 
-    mov dword [border], 0 ; initialize border to 0
     mov eax, [sampling_window_size]
     mov ebx, 2
     div ebx
     dec eax
     mov [border], eax
 
-
-for1:
+for_row:
     mov eax, [i]
     cmp eax, [image_size_x]
-    jge for1_end
+    jge for_row_end
 
     mov dword [j], 0
 
-    for2:
-        mov eax, [j]
-        cmp eax, [image_size_y]
-        jge for2_end
+for_col:
+    mov eax, [j]
+    cmp eax, [image_size_y]
+    jge for_col_end
 
-        ; if conditions
-        ; i <= 0
+    mov eax, [i]
+    cmp eax, [border]
+    jle true_condition
+
+    mov ebx, [image_size_x]
+    dec ebx
+    sub ebx, [border]
+    cmp eax, ebx
+    jge true_condition
+
+    mov eax, [j]
+    cmp eax, [border]
+    jle true_condition
+
+    mov ebx, [image_size_y]
+    dec ebx
+    sub ebx, [border]
+    cmp eax, ebx
+    jge true_condition
+
+    jmp false_condition
+
+    true_condition:
         mov eax, [i]
-        cmp eax, [border]
-        jle true
-
-        ; i >= image_size_x - border - 1
         mov ebx, [image_size_x]
-        dec ebx
-        sub ebx, [border]
-        cmp eax, ebx
-        jge true
+        mul ebx
+        add eax, [j]
+        shl eax, 2
+        mov esi, [input_image_pointer]
+        add esi, eax
 
-        ; j == 0
-        mov eax, [j]
-        cmp eax, [border]
-        jle true
+        mov edi, [filtered_image_pointer]
+        add edi, eax
 
-        ; j >= image_size_y - border - 1
-        mov ebx, [image_size_y]
-        dec ebx
-        sub ebx, [border]
-        cmp eax, ebx
-        jge true
+        mov eax, [esi]
+        mov [edi], eax
+        jmp end_if_condition
 
-        jmp false
-        ; if
-        true:
-            ; input_image[i * image_size_x + j]
+    false_condition:
+        mov dword [sum], 0
+        mov dword [k], 0 
+
+for_row_sampling:
+        mov eax, [k]
+        cmp eax, [sampling_window_size]
+        jge for_row_sampling_end
+
+        mov dword [l], 0
+
+for_col_sampling:
+            mov eax, [l]
+            cmp eax, [sampling_window_size]
+            jge for_col_sampling_end
+
             mov eax, [i]
+            mov ecx, [border]
+            inc ecx
+            sub eax, ecx
+            add eax, [k]
             mov ebx, [image_size_x]
             mul ebx
-            add eax, [j]
+            mov ebx, [j]
+            sub ebx, ecx
+            add ebx, [l]
+            add eax, ebx
             shl eax, 2
             mov esi, [input_image_pointer]
             add esi, eax
 
-            ; filtered_image[i * image_size_x + j]
-            mov edi, [filtered_image_pointer]
-            add edi, eax
+            mov eax, [sum]
+            add eax, [esi]
+            mov [sum], eax
 
-            ; placing value of input_image into filtered
-            mov eax, [esi]
-            mov [edi], eax
-            jmp end_if
+            inc dword [l]
+            jmp for_col_sampling
 
-        false:
-            mov dword [sum], 0
-            
-            mov dword [k], 0 
+for_col_sampling_end:
+        inc dword [k]
+        jmp for_row_sampling
 
-            for3:
-                mov eax, [k]
-                cmp eax, [sampling_window_size]
-                jge for3_end
+for_row_sampling_end:
+        mov eax, [sampling_window_size]
+        mov ebx, [sampling_window_size]
+        mul ebx
+        mov ebx, eax
+        mov eax, [sum]
+        mov ecx, ebx
+        shr ebx, 1
+        add eax, ebx
+        mov ebx, ecx
+        div ebx
 
-                mov dword [l], 0
-
-                for4:
-                    mov eax, [l]
-                    cmp eax, [sampling_window_size]
-                    jge for4_end
-
-                    ; input_image[(i-(border + 1) + k) * image_size_x + (j-(border + 1) + l)]
-                    mov eax, [i] ; i
-                    mov ecx, [border] ; border 
-                    inc ecx ; + 1
-                    sub eax, ecx ; i - (border + 1)
-                    add eax, [k] ; + k
-                    mov ebx, [image_size_x]
-                    mul ebx ; * image_size_x
-                    mov ebx, [j]
-                    sub ebx, ecx ; j - (border + 1)
-                    add ebx, [l]
-                    add eax, ebx
-                    shl eax, 2
-                    mov esi, [input_image_pointer]
-                    add esi, eax
-
-                    ; sum += 
-                    mov eax, [sum]
-                    add eax, [esi]
-                    mov [sum], eax
-
-                    inc dword [l]
-                    jmp for4
-
-                for4_end:
-
-                    inc dword [k]
-                    jmp for3
-
-            for3_end:
-                ; sum / (sampling_window_size * sampling_window_size) with round to nearest, ties away from zero
-                mov eax, [sampling_window_size]
-                mov ebx, [sampling_window_size]
-                mul ebx
-                mov ebx, eax
-                mov eax, [sum]
-                mov ecx, ebx
-                shr ebx, 1
-                add eax, ebx
-                mov ebx, ecx
-                div ebx
-
-                ; filtered_image[i * (image_size_x) + j]
-                mov ecx, eax
-                mov eax, [i]
-                mov ebx, [image_size_x]
-                mul ebx
-                add eax, [j]
-                shl eax, 2
-                mov edi, [filtered_image_pointer]
-                add edi, eax
-
-                ; placing value of calculation into filtered_image
-                mov eax, ecx
-                mov [edi], eax
-                jmp end_if
-
-        end_if:
-            mov eax, [j]
-            inc eax
-            mov [j], eax
-            jmp for2
-
-    for2_end:
+        mov ecx, eax
         mov eax, [i]
+        mov ebx, [image_size_x]
+        mul ebx
+        add eax, [j]
+        shl eax, 2
+        mov edi, [filtered_image_pointer]
+        add edi, eax
+
+        mov eax, ecx
+        mov [edi], eax
+
+end_if_condition:
+        mov eax, [j]
         inc eax
-        mov [i], eax
-        jmp for1
+        mov [j], eax
+        jmp for_col
 
-for1_end:
+for_col_end:
+    mov eax, [i]
+    inc eax
+    mov [i], eax
+    jmp for_row
 
+for_row_end:
     mov esp, ebp
     pop ebp
     ret
-    
